0b2654f6994940d85ff4a21e8caef9cbee48e6b3
Tan Quach <t@nqua.ch>
2017-08-07 10:15:02 +0100

    Updated idea launch script

--- idea.sh
+++ idea.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -dit --rm -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v $HOME/develop:/home/user/code -v $HOME/.IntelliJIdea2017.2-docker:/home/user/.IntelliJIdea2017.2 --name=idea guoguojin/ubuntu-jdk-intellij:2017.2.1
+docker run -d -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v $HOME/develop:/home/user/code -v $HOME/.IntelliJIdea2017.2-docker:/home/user/.IntelliJIdea2017.2 -v $HOME/.m2:/home/user/.m2 -v $HOME/.ivy2:/home/user/.ivy2 --name=idea birchwoodlangham/ubuntu-jdk-intellij:2017.2.1
